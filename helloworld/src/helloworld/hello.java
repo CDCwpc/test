df24025fd49728198b88b1317e5d4506463fd8f2
CDCwpc <wpc@cndatacom.com>
2019-04-04 10:23:00 +0800

    本地库测试

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("1231231231");
+        System.out.println("本地库测试");
     }
 }
