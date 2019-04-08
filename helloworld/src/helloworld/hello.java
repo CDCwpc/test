1e6d374dfc24eabd34c0f9c52cbcc72c8ec00f26
CDCwpc <wpc@cndatacom.com>
2019-04-08 09:49:05 +0800

    Update hello.java

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -3,7 +3,8 @@ package helloworld;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("1231231231");
+       
         System.out.println("本地库测试");
+        
     }
 }
