065cca5535d38acde966111f7fedee14ae5ee343
CDCwpc <wpc@cndatacom.com>
2019-04-08 14:37:28 +0800

    创建wpc分支

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -3,7 +3,7 @@ package helloworld;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-       
+        System.out.println("分支测试");
         System.out.println("本地库测试");
         
     }
